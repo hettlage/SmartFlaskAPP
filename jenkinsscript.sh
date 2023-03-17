106f2a35eccc3b32ff3f3fdb181a76c99f8593e4
Christian Hettlage <hettlage@saao.ac.za>
2023-03-17 23:00:15 +0200

    Update Jenkins pipeline

--- jenkinsscript.sh
+++ jenkinsscript.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 . ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+xport PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
 
 pyenv local 3.10.0
 python3 -m venv venv
